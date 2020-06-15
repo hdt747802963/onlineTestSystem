8b9353bede7664c921bd7f738f822cde0688d4be
cm <1148858099@qq.com>
2020-06-15 10:49:37 +0800

    test

---
+++ src/main/java/com/olts/LoginInterceptor.java
@@ -0,0 +1,58 @@
+package com.olts;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import net.sf.json.JSONObject;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.olts.entity.User;
+
+
+public class LoginInterceptor  implements HandlerInterceptor{
+
+	@Override
+	public void afterCompletion(HttpServletRequest arg0,
+			HttpServletResponse arg1, Object arg2, Exception arg3)
+			throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest arg0, HttpServletResponse arg1,
+			Object arg2, ModelAndView arg3) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
+			Object arg2) throws Exception {
+		// TODO Auto-generated method stub
+		String url = request.getRequestURI();
+		//System.out.println("进入拦截器，url = " + url);
+		Object user = request.getSession().getAttribute("user");
+		if(user == null){
+			//表示未登录或者登录状态失效
+			System.out.println("未登录或登录失效，url = " + url);
+			if("XMLHttpRequest".equals(request.getHeader("X-Requested-With"))){
+				//ajax
+				Map<String, String> ret = new HashMap<String, String>();
+				ret.put("type", "error");
+				ret.put("msg", "登录状态已失效，请重新去登录!");
+				response.getWriter().write(JSONObject.fromObject(ret).toString());
+				return false;
+			}
+			response.sendRedirect(request.getContextPath() + "/system/login");
+			return false;
+		}
+		return true;
+	}
+
+}
---
+++ src/main/java/com/olts/entity/User.java
@@ -0,0 +1,30 @@
+package com.olts.entity;
+
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class User {
+	private Long id;//用户id，主键、自增
+	private String username;//
+	private String password;//
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+}
